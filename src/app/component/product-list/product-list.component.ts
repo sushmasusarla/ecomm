552241140d0cf33dd2d55252524ed8cc9a430c53
sushmasusarla <168928295+sushmasusarla@users.noreply.github.com>
2024-09-09 12:38:54 +0530

    Add files via upload

---
+++ src/app/component/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/service/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent  implements OnInit{
+  products: any[] = [];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+    });
+  }
+  addToCart(product: any) {
+    if (product.name === "Women's Saree") {
+      this.productService.addToCart(product);
+    }
+  }
+
+
+}
