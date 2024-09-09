b68584632e768b56a27046076e77b12c50311bf9
sushmasusarla <168928295+sushmasusarla@users.noreply.github.com>
2024-09-09 12:24:15 +0530

    Add files via upload

---
+++ src/app/component/cart/cart.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/service/product.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent  implements OnInit{
+
+  cart: any[] = [];
+
+  constructor(private productService: ProductService) { }
+
+
+  ngOnInit(): void {
+    this.cart = this.productService.getCart();
+  }
+
+}
