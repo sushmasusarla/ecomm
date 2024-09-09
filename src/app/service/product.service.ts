cb256f965504bff1663f43990a413db45fd678dc
sushmasusarla <168928295+sushmasusarla@users.noreply.github.com>
2024-09-09 12:40:56 +0530

    Add files via upload

---
+++ src/app/service/product.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+   productsUrl = 'assets/products.json';
+   cart: any[] = [];
+
+  
+
+  constructor(private http:HttpClient) { }
+
+  getProducts(): Observable<any[]> {
+    return this.http.get<any[]>(this.productsUrl);
+  }
+  addToCart(product: any) {
+    this.cart.push(product);
+    console.log('Cart:', this.cart);
+  }
+  getCart(): any[] {
+    return this.cart;
+  }
+}
