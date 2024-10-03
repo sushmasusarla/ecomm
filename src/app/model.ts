912b51d1a44c9f57712cd90246d8f0460294894a
sushmasusarla <168928295+sushmasusarla@users.noreply.github.com>
2024-10-03 12:44:36 +0530

    Add files via upload

---
+++ src/app/model.ts
@@ -0,0 +1,14 @@
+export interface datamodal{
+    id: number;
+  name: string;
+  descripton: string ;
+  price: number;
+  image: string;
+  details: string;
+  size: string;
+  offers: string;
+  reviwes:string []
+  
+  
+    
+}
