2f0110f70b98eec82c7d309332f5b5fd6a51ea4c
sushmasusarla <168928295+sushmasusarla@users.noreply.github.com>
2024-10-24 21:49:30 +0530

    Update home.component.ts

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component,ElementRef, ViewChild } from '@angular/core';
+import { ProductsService } from 'src/app/service/product.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  private apiUrl = 'assets/products.json';  
+  products: any[] = [];
+  constructor(private productService: ProductsService) {}
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+    });    
+
+    }
+     // Data for cards
+  cards = [
+    { title: 'Card 1', description: 'This is the first card.' },
+    { title: 'Card 2', description: 'This is the second card.' },
+    { title: 'Card 3', description: 'This is the third card.' },
+    { title: 'Card 4', description: 'This is the fourth card.' }
+  ];
 }
